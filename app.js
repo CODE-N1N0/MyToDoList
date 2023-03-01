52e8ce76e1da94327efc2708787981c469343ea3
CODE-N1N0 <sibelleflrs@gmail.com>
2023-03-01 16:37:27 +0800

    initial commit of my ToDoList app.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const https = require("https");
 
+
 const app = express();
+//app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("Public"));
 
-app.get("/",  function(req,res){
-  res.sendFile(__dirname + "Public/index.html");
-});
 
+app.get("/", function(req,res){
+res.sendFile(__dirname + "Public/index.html");
+});
 
 app.listen(3000, function(){
   console.log("server is up and running on port 3000 for your To Do List. Good job! Keep going! Don't give up!");
